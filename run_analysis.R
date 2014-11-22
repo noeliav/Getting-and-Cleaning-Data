5f0bf0e2c12d301145dfa9501cec2fc05c74a37e
noeliav <noelia.viles@gmail.com>
2014-11-22 13:57:56 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -84,4 +84,4 @@ melt_data = melt(data, id=id_labels, measure.vars=data_labels)
 
 tidy_data = dcast(melt_data, subject+ Activity_Label ~ variable, mean)
 
-write.table(tidy_data, file="~/tidy_data.txt")
+write.table(tidy_data, file="./tidy_data.txt")
