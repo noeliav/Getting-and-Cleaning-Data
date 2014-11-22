5f5e815ac64cdff599eca5f2e12002ce655de00c
noeliav <noelia.viles@gmail.com>
2014-11-22 13:57:36 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -16,11 +16,11 @@ require("data.table")
 
 # Load: activity labels
 
-activity_labels<-read.table("~/UCI HAR Dataset/activity_labels.txt")[,2]
+activity_labels<-read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
 
 # Load: features
 
-features<-read.table("~/UCI HAR Dataset/features.txt")[,2]
+features<-read.table("./UCI HAR Dataset/features.txt")[,2]
 
 #Extracts only the measurements on the mean and standard deviation for each measurement.
 
@@ -28,10 +28,10 @@ extract_features<-grepl("mean|std", features)
 
 #Load X_test and y_test data
 
-X_test<-read.table("~/UCI HAR Dataset/test/X_test.txt")
-y_test<-read.table("~/UCI HAR Dataset/test/y_test.txt")
+X_test<-read.table("./UCI HAR Dataset/test/X_test.txt")
+y_test<-read.table("./UCI HAR Dataset/test/y_test.txt")
 
-subject_test<-read.table("~/UCI HAR Dataset/test/subject_test.txt")
+subject_test<-read.table("./UCI HAR Dataset/test/subject_test.txt")
 
 names(X_test)=features
 
@@ -52,10 +52,10 @@ test_data<-cbind(as.data.table(subject_test), y_test, X_test)
 
 # Load X_train and y_train data
 
-X_train<-read.table("~/UCI HAR Dataset/train/X_train.txt")
-y_train<-read.table("~/UCI HAR Dataset/train/y_train.txt")
+X_train<-read.table("./UCI HAR Dataset/train/X_train.txt")
+y_train<-read.table("./UCI HAR Dataset/train/y_train.txt")
 
-subject_train<-read.table("~/UCI HAR Dataset/train/subject_train.txt")
+subject_train<-read.table("./UCI HAR Dataset/train/subject_train.txt")
 
 names(X_train)=features
 
